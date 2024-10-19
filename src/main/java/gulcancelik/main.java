4b970e324c25e45b161e8cf0930513a2c44af5c7
gulcannce <gulcanncee@gmail.com>
2024-10-19 20:48:58 +0300

    Initial commit

---
+++ src/main/java/gulcancelik/main.java
@@ -0,0 +1,14 @@
+package gulcancelik;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.print("Merhaba");
+        System.out.print(" Dünya");
+
+    }
+
+}
+
